a7254090958e320b89151fe1976fe52a5cd3be0d
Igor <igor.reisleandro@gmail.com>
2022-10-12 02:11:06 -0300

    test: add test cases

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,18 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+
+    test "should return exact number of odd numbers" do
+      assert ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"]) == 3
+    end
+
+    test "should consider decimal values as odd" do
+      assert ListFilter.call(["1", "3", "6", "43", "banana", "6.64", "abc"]) == 4
+    end
+
+    test "should return 0 for empty list" do
+      assert ListFilter.call([]) == 0
+    end
   end
 end
